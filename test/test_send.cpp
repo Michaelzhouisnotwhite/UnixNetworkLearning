77a991f760978a7a6dbaa626de2e44c5259ef914
Michael <zyqing601@163.com>
2024-03-18 18:13:05 +0800

    update

--- test/test_send.cpp
+++ test/test_send.cpp
@@ -1,7 +1,7 @@
 #include "net.h"
 
 int main(int argc, char* argv[]) {
-  auto status = Send2Server("0.0.0.0", 8089, "abc");
+  auto status = Send2Server("0.0.0.0", 8089, "Hello From Client");
   if (!status.ok()) {
     std::cout << status.status() << "\n";
   }else {
